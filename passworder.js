9b816dc5e0267cc4fe8e44fc25556c4d0a5513d9
dkpsystem <dkpsystem@dkptest.dkptest.com>
2010-05-20 18:07:30 -0500

    Works version 1, I guess

--- passworder.js
+++ passworder.js
@@ -46,8 +46,10 @@ function tr(body)
 	return "<tr>" + body + "</tr";
 }
 
-function td(body,colspan=1)
+function td(body,colspan)
 {
+	if(typeof colspan=="undefined")
+		colspan=1;
 	return "<td colspan=" + colspan + ">" + body + "</td>";
 }
 
@@ -55,13 +57,196 @@ function input(id,onchange,def)
 {
 	if(typeof def=="undefined")
 		def = "";
-	return "<input type=text value=\"" + def + "\" id=\"" + id + "\" onchange=\"" + onchange + "\" />";
+	return "<input type=text value=\"" + def + "\" id=\"" + id + "\" onkeyup=\"" + onchange + "\" />";
 }
 
 function checkbox(id,onchange)
 {
-	return "<input type=checkbox id=" + id + " onchange=\"" + onchange + "\" />";
+	return "<input type=checkbox id=" + id + " onclick=\"" + onchange + "\" />";
 }
+
+function clear_history()
+{
+	dge("history").innerHTML = "";
+}
+
+function add_history(v)
+{
+	dge("history").innerHTML += v + "<br />";
+}
+
+function calculate_pass(interleaved,base,length,attemptsleft)
+{
+	if(typeof attemptsleft == "undefined")
+		attemptsleft = 10;
+
+	if(attemptsleft==10)
+		clear_history();
+
+	if(isNaN(length))
+		return "";
+	else
+		length = parseInt(length);
+
+	if(length<1)
+		return "";
+
+
+	//alert(length);
+	
+	var numberized = convert_to_number(interleaved);
+	var result = to_base(base,numberized);
+
+	add_history(numberized + ": " + result);
+
+	if(interleaved == "")
+		return "";
+	else if(result.length == 0)
+		return "Something Went Wrong";
+	else if(result.length < length)
+	{
+		//alert("Too short:" + result + "attemptsleft: " + attemptsleft);
+		return calculate_pass(interleaved + interleaved,base,length,attemptsleft-1);
+	}
+	else if(result.length > length)
+	{
+		//alert("Too long:" + result + "attmptsleft: " + attemptsleft);
+		if(attemptsleft > 0)
+			return calculate_pass(combine_every_other(interleaved),base,length,attemptsleft-1);
+		else
+			return result.substr(0,length);
+	}
+	else
+		return result;
+}
+		
+function combine_every_other(s)
+{
+	var retstr = "";
+	if(s.length<=1)
+	{
+		return String.fromCharCode(s.charCodeAt(0)/2);
+	}
+	for(i=0;i<s.length;i+=2)
+	{
+		var num = (s.charCodeAt(i) + s.charCodeAt(i+1))%256;
+		retstr += String.fromCharCode(num);
+	}
+	return retstr;
+}
+
+
+
+
+function passworder_calc_pass()
+{
+	var salt = dge("salt").value;
+	var password = dge("password").value;
+	var length = parseInt(dge("length").value);
+	var specialchars = dge("specialchars").checked;
+	var base = specialchars ? 78 : 62; //62 = alphanumeric only, 78 = add special chars
+
+	
+	var interleaved = interleave_salt_and_pw(salt,password);
+	var finalpw = calculate_pass(interleaved,base,length);
+
+	dge("finalpw").innerHTML = finalpw;
+}
+
+/*function cut_to_length(str,len)
+{
+	if(str.length > len)
+*/	
+
+function interleave_salt_and_pw(salt,pw)
+{
+	var retstr = "";
+	var max = Math.max(salt.length,pw.length);
+	for(var i=0;i<max;i++)
+		retstr += ith_char(i,salt) + ith_char(i,pw);
+	return retstr;
+}
+
+function ith_char(i,str)
+{
+	if(i>=str.length)
+		i %= str.length;
+	return str.charAt(i);
+}
+
+
+function convert_to_number(s)
+{
+	var num=0;
+	for(var i=0;i<s.length;i++)
+		num += s.charCodeAt(i) * Math.pow(256,i);
+	return num;	
+}
+
+function log_base(base,num)
+{
+	return Math.log(num)/Math.log(base);
+}
+
+// b is the base, in our case, either 62 for alphanumeric only or 78 for non-alphanumeric only
+function to_base(base,num)
+{
+	var retstr = "";
+	var curval = 0;
+	var log = Math.floor(log_base(base,num));
+	while(log>=0)
+	{
+		var mult = Math.pow(base,log);
+		curval = Math.floor(num/mult);
+		retstr += onechar(curval);
+		num -= curval*mult;
+		log--;
+	}
+	return retstr;
+}
+
+
+
+function onechar(num)
+{
+	if(num>=0 && num<=9)
+		return "" + num; // 0-9
+	else{
+		num -= 10;
+		if(num>=0 && num<=25)
+			return String.fromCharCode(num + 65); // A-Z
+		else {
+			num -= 26;
+			if(num>=0 && num<=25)
+				return String.fromCharCode(num+97); // a-z
+			else {
+				num -= 26;
+				switch(num)
+				{
+					case 0: return "-";
+					case 1: return "+";
+					case 2: return "=";
+					case 3: return "_";
+					case 4: return "?";
+					case 5: return ".";
+					case 6: return ",";
+					case 7: return ";";
+					case 8: return ":";
+					case 9: return "@";
+					case 10: return "$";
+					case 11: return "%";
+					case 12: return "!";
+					case 13: return "~";
+					case 14: return "*";
+					case 15: return "^";
+					default: alert("out of range");return ;
+				}
+			}
+		}
+	}
+}
+
+
 
 
 
@@ -70,7 +255,7 @@ function print_form()
 	fun = "passworder_calc_pass()";
 	w(table(
 		tr(
-			td("<b>passworder.js</b> - Password Generator<br /><a href='http://github.com/choptastic/passworder' target=_new>http://github.com/choptastic/passworder</a>")
+			td("<b>passworder.js</b> - Password Generator<br /><a href='http://github.com/choptastic/passworder.js' target=_new>http://github.com/choptastic/passworder.js</a>",2)
 		) +
 		tr(
 			td(label("Salt:","A word, phrase, or other string of charcters that you can easily remember.  This will be the, in a sense, 'the password to your passwords'")) +
@@ -90,7 +275,11 @@ function print_form()
 		) +
 		tr(
 			td(label("Generated Password:","This is the final password to enter into password fields on webpages and programs.")) +
-			td("<span id=\"final_pw\" style='font-size:larger;font-weight:bold;font-family:monospace'></span>")
+			td("<span id=\"finalpw\" style='font-size:larger;font-weight:bold;font-family:monospace'></span>")
+		)
+		+ tr(
+			td(label("Result History:","Can be ignored")) +
+			td("<span id='history' style='font-family:monospace'></span>")
 		)
 	));
 

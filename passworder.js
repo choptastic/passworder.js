7fb44a45f9ea904b50034e7fbec986bd50bcf84f
dkpsystem <dkpsystem@dkptest.dkptest.com>
2010-05-21 12:23:24 -0500

    Made it run in a class to avoid any scoping conflicts

--- passworder.js
+++ passworder.js
@@ -21,267 +21,237 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE. */
 
-function dge(e)
-{
-	return document.getElementById(e);
-}
-
-function w(m)
-{
-	document.write(m);
-}
-
-function label(fieldname,description)
-{
-	return fieldname + "<br /><i style='font-size:smaller;color:grey'>" + description + "</i>";
-}
-
-function table(body)
-{
-	return "<table>" + body + "</table>";
-}
-
-function tr(body)
-{
-	return "<tr>" + body + "</tr";
-}
-
-function td(body,colspan)
-{
-	if(typeof colspan=="undefined")
-		colspan=1;
-	return "<td colspan=" + colspan + ">" + body + "</td>";
-}
-
-function input(id,onchange,def)
-{
-	if(typeof def=="undefined")
-		def = "";
-	return "<input type=text value=\"" + def + "\" id=\"" + id + "\" onkeyup=\"" + onchange + "\" />";
-}
-
-function checkbox(id,onchange)
-{
-	return "<input type=checkbox id=" + id + " onclick=\"" + onchange + "\" />";
-}
-
-function clear_history()
-{
-	if(dge("history"))
-		dge("history").innerHTML = "";
-}
-
-function add_history(v)
-{
-	if(dge("history"))
-		dge("history").innerHTML += v + "<br />";
-}
-
-function calculate_pass(interleaved,base,length,attemptsleft)
-{
-	add_history("----------------------------------");
-	if(typeof attemptsleft == "undefined")
+var passworder = {
+	version: "0.1",
+	dge : function(e)
 	{
-		attemptsleft = 5+length;
-		clear_history();
-	}
-
-	if(isNaN(length))
-		return "";
-	else
-		length = parseInt(length);
-
-	if(length<1)
-		return "";
-
-	var numberized = convert_to_number(interleaved);
-	var result = to_base(base,numberized);
-
-	add_history("Pass " + attemptsleft + " - " + interleaved + " = " + numberized + " --> " + result);
-
-	if(interleaved == "")
-		return "";
-	else if(result.length == 0)
-		return "Something Went Wrong";
-	else if(result.length < length)
+		return document.getElementById(e);
+	},
+	w: function(m)
 	{
-		add_history("Too short:" + result.length + " < " + length);
-		return calculate_pass(interleaved + interleaved,base,length,attemptsleft);
-	}
-	else if(result.length > length)
+		document.write(m);
+	},
+
+	label: function(fieldname,description)
 	{
-		add_history("Too Long:" + result.length + " > " + length);
-		if(attemptsleft > 0)
-			return calculate_pass(combine_every_other(interleaved),base,length,attemptsleft-1);
+		return fieldname + "<br /><i style='font-size:smaller;color:grey'>" + description + "</i>";
+	},
+
+	table: function(body)
+	{
+		return "<table>" + body + "</table>";
+	},
+
+	tr: function(body)
+	{
+		return "<tr>" + body + "</tr";
+	},
+
+	td: function(body,colspan)
+	{
+		if(typeof colspan=="undefined")
+			colspan=1;
+		return "<td colspan=" + colspan + ">" + body + "</td>";
+	},
+	input: function(id,onchange,def)
+	{
+		if(typeof def=="undefined")
+			def = "";
+		return "<input type=text value=\"" + def + "\" id=\"" + id + "\" onkeyup=\"" + onchange + "\" />";
+	},
+	checkbox: function(id,onchange)
+	{
+		return "<input type=checkbox id=" + id + " onclick=\"" + onchange + "\" />";
+	},
+
+	clear_history: function()
+	{
+		if(this.dge("history"))
+			this.dge("history").innerHTML = "";
+	},
+	add_history: function(v)
+	{
+		if(this.dge("history"))
+			this.dge("history").innerHTML += v + "<br />";
+	},
+
+	calculate_pass: function(numberized,base,length,attemptsleft)
+	{
+		this.add_history("----------------------------------");
+		if(typeof attemptsleft == "undefined")
+		{
+			attemptsleft = length*2;
+			this.clear_history();
+		}
+
+		if(length>49)
+			return "Please keep password length under 50";
+
+		if(isNaN(length))
+			return "";
 		else
-			return result.substr(0,length);
-	}
-	else
-		return result;
-}
+			length = parseInt(length);
+
+		if(length<1)
+			return "";
+
+		var result = this.to_base(base,numberized);
+
+		this.add_history("Pass " + attemptsleft + " - " + numberized + " --> " + result);
+
+		if(numberized==0)
+			return "Nothing Yet";
+		else if(result.length == 0)
+			return "Something Went Wrong";
+		else if(result.length < length)
+		{
+			this.add_history("Too short:" + result.length + " < " + length);
+			return this.calculate_pass(numberized*(attemptsleft+1),base,length,attemptsleft);
+		}
+		else if(result.length > length)
+		{
+			this.add_history("Too Long:" + result.length + " > " + length);
+			if(attemptsleft > 0)
+				return this.calculate_pass(Math.floor(Math.sqrt(numberized)),base,length,attemptsleft-1);
+			else
+				return result.substr(0,length);
+		}
+		else
+			return result;
+	},
+	passworder_calc_pass: function()
+	{
+		var salt = this.dge("salt").value;
+		var password = this.dge("password").value;
+		var length = parseInt(this.dge("length").value);
+		var specialchars = this.dge("specialchars").checked;
+		var base = specialchars ? 78 : 62; //62 = alphanumeric only, 78 = add special chars
+
 		
-function combine_every_other(s)
-{
-	var retstr = "";
-	if(s.length<=1)
+		var interleaved = this.interleave_salt_and_pw(salt,password);
+		var numberized = this.convert_to_number(interleaved);
+		var finalpw = this.calculate_pass(numberized,base,length);
+
+		this.dge("finalpw").innerHTML = finalpw;
+	},
+
+	interleave_salt_and_pw: function(salt,pw)
 	{
-		return String.fromCharCode(s.charCodeAt(0)/2);
-	}
-	for(i=0;i<s.length;i+=2)
+		var retstr = "";
+		var max = Math.max(salt.length,pw.length);
+		var i;
+		for(i=0;i<max;i++)
+			retstr += this.ith_char(i,salt) + this.ith_char(i,pw);
+		return retstr;
+	},
+
+	ith_char: function(i,str)
 	{
-		var num = (s.charCodeAt(i) + s.charCodeAt(i+1))%256;
-		retstr += String.fromCharCode(num);
-	}
-	return retstr;
-}
+		if(i>=str.length)
+			i %= str.length;
+		return str.charAt(i);
+	},
 
-
-
-
-function passworder_calc_pass()
-{
-	var salt = dge("salt").value;
-	var password = dge("password").value;
-	var length = parseInt(dge("length").value);
-	var specialchars = dge("specialchars").checked;
-	var base = specialchars ? 78 : 62; //62 = alphanumeric only, 78 = add special chars
-
+	convert_to_number: function(s)
+	{
+		var num=0;
+		for(var i=0;i<s.length;i++)
+			num += s.charCodeAt(i) * Math.pow(256,i);
+		return num;	
+	},
 	
-	var interleaved = interleave_salt_and_pw(salt,password);
-	var finalpw = calculate_pass(interleaved,base,length);
-
-	dge("finalpw").innerHTML = finalpw;
-}
-
-/*function cut_to_length(str,len)
-{
-	if(str.length > len)
-*/	
-
-function interleave_salt_and_pw(salt,pw)
-{
-	var retstr = "";
-	var max = Math.max(salt.length,pw.length);
-	for(var i=0;i<max;i++)
-		retstr += ith_char(i,salt) + ith_char(i,pw);
-	return retstr;
-}
-
-function ith_char(i,str)
-{
-	if(i>=str.length)
-		i %= str.length;
-	return str.charAt(i);
-}
-
-
-function convert_to_number(s)
-{
-	var num=0;
-	for(var i=0;i<s.length;i++)
-		num += s.charCodeAt(i) * Math.pow(256,i);
-	return num;	
-}
-
-function log_base(base,num)
-{
-	return Math.log(num)/Math.log(base);
-}
-
-// b is the base, in our case, either 62 for alphanumeric only or 78 for non-alphanumeric only
-function to_base(base,num)
-{
-	var retstr = "";
-	var curval = 0;
-	var log = Math.floor(log_base(base,num));
-	while(log>=0)
+	log_base: function(base,num)
 	{
-		var mult = Math.pow(base,log);
-		curval = Math.floor(num/mult);
-		retstr += onechar(curval);
-		num -= curval*mult;
-		log--;
-	}
-	return retstr;
-}
+		return Math.log(num)/Math.log(base);
+	},
 
-
-
-function onechar(num)
-{
-	if(num>=0 && num<=9)
-		return "" + num; // 0-9
-	else{
-		num -= 10;
-		if(num>=0 && num<=25)
-			return String.fromCharCode(num + 65); // A-Z
-		else {
-			num -= 26;
+	// b is the base, in our case, either 62 for alphanumeric only or 78 for non-alphanumeric only
+	to_base: function(base,num)
+	{
+		var retstr = "";
+		var curval = 0;
+		var log = Math.floor(this.log_base(base,num));
+		while(log>=0)
+		{
+			var mult = Math.pow(base,log);
+			curval = Math.floor(num/mult);
+			retstr += this.onechar(curval);
+			num -= curval*mult;
+			log--;
+		}
+		return retstr;
+	},
+	onechar: function(num)
+	{
+		if(num>=0 && num<=9)
+			return "" + num; // 0-9
+		else{
+			num -= 10;
 			if(num>=0 && num<=25)
-				return String.fromCharCode(num+97); // a-z
+				return String.fromCharCode(num + 65); // A-Z
 			else {
 				num -= 26;
-				switch(num)
-				{
-					case 0: return "-";
-					case 1: return "+";
-					case 2: return "=";
-					case 3: return "_";
-					case 4: return "?";
-					case 5: return ".";
-					case 6: return ",";
-					case 7: return ";";
-					case 8: return ":";
-					case 9: return "@";
-					case 10: return "$";
-					case 11: return "%";
-					case 12: return "!";
-					case 13: return "~";
-					case 14: return "*";
-					case 15: return "^";
-					default: alert("out of range");return ;
+				if(num>=0 && num<=25)
+					return String.fromCharCode(num+97); // a-z
+				else {
+					num -= 26;
+					switch(num)
+					{
+						case 0: return "-";
+						case 1: return "+";
+						case 2: return "=";
+						case 3: return "_";
+						case 4: return "?";
+						case 5: return ".";
+						case 6: return ",";
+						case 7: return ";";
+						case 8: return ":";
+						case 9: return "@";
+						case 10: return "$";
+						case 11: return "%";
+						case 12: return "!";
+						case 13: return "~";
+						case 14: return "*";
+						case 15: return "^";
+						default: alert("out of range");return ;
+					}
 				}
 			}
 		}
+	},
+	print_form: function()
+	{
+		fun = "passworder.passworder_calc_pass()";
+		this.w(this.table(
+			this.tr(
+				this.td("<div class=pw_title style='text-align:center'><b>passworder.js</b> (v" + this.version + ") - Password Generator<br /><a href='http://github.com/choptastic/passworder.js' target=_new>http://github.com/choptastic/passworder.js</a></div>",2)
+			) +
+			this.tr(
+				this.td(this.label("Salt:","A word, phrase, or other string of charcters that you can easily remember. <br />This will be the, in a sense, \"the password to your passwords\"")) +
+				this.td(this.input("salt",fun))
+			) +
+			this.tr(
+				this.td(this.label("Password:","This is the actual password you wish to use.  You should only<br />use this password in one place, to be safe.")) +
+				this.td(this.input("password",fun))
+			) +
+			this.tr(
+				this.td(this.label("Desired Password Length:","In number of characters")) +
+				this.td(this.input("length",fun,10))
+			) +
+			this.tr(
+				this.td(this.label("Allow non-alphanumeric characters in the password?","Such as +, ?, ;, :, =, etc..</i>")) +
+				this.td(this.checkbox("specialchars",fun))
+			) +
+			this.tr(
+				this.td("<div class=pw_result style='text-align:center'>Generated Password:<br /><span id=\"finalpw\" style='font-size:24pt;font-weight:bold;font-family:monospace'>Nothing Yet</span>",2)
+			)
+			/*+ tr(
+				td(label("Result History:","Can be ignored")) +
+				td("<div id='history' style='height:200px;overflow:auto;font-family:monospace'></span>")
+			)*/
+		));
+
 	}
 }
 
-
-
-
-
-function print_form()
-{
-	fun = "passworder_calc_pass()";
-	w(table(
-		tr(
-			td("<div class=pw_title><b>passworder.js</b> - Password Generator<br /><a href='http://github.com/choptastic/passworder.js' target=_new>http://github.com/choptastic/passworder.js</a></div>",2)
-		) +
-		tr(
-			td(label("Salt:","A word, phrase, or other string of charcters that you can easily remember. <br />This will be the, in a sense, \"the password to your passwords\"")) +
-			td(input("salt",fun))
-		) +
-		tr(
-			td(label("Password:","This is the actual password you wish to use.  You should only<br />use this password in one place, to be safe.")) +
-			td(input("password",fun))
-		) +
-		tr(
-			td(label("Desired Password Length:","In number of characters")) +
-			td(input("length",fun,10))
-		) +
-		tr(
-			td(label("Allow non-alphanumeric characters in the password?","Such as +, ?, ;, :, =, etc..</i>")) +
-			td(checkbox("specialchars",fun))
-		) +
-		tr(
-			td("<div class=pw_result style='text-align:center'>Generated Password:<br /><span id=\"finalpw\" style='font-size:24pt;font-weight:bold;font-family:monospace'>Nothing Yet</span>",2)
-		)
-		+ tr(
-			td(label("Result History:","Can be ignored")) +
-			td("<span id='history' style='font-family:monospace'></span>")
-	  	)
-	));
-
-}
-
-print_form();
+passworder.print_form();
